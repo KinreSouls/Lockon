e69046792199d192c5c14b8a915aea2638ad3fc7
unknown <juliajk060@gmal.com>
2023-02-21 19:38:33 -0500

    Lockon

---
+++ LockonKinre.lua
@@ -0,0 +1,13 @@
+--                  [[KinreSouls Lockon]]              
+--[[lockon for pvp press the key ( P ) to activate the script]] 
+--[[and press the key ( V ) to aim at the player and unmount]]
+--[[Made by KinreSouls / ⸙ ᴊᴜʟɪᴀɴ¹¹ ]]
+--[[Discord Server : http://discord.gg/cUSrqMSsY2]]
+
+ Activated = "p";
+ KeyAimbot = "v";
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KinreSouls/AutoSlotV1/main/Lockon.lua"))()
